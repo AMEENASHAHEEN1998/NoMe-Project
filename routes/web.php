22b29fa86603c803052e76c38e4fa75547bc968e
gadeerdawwas <40713088+gadeerdawwas@users.noreply.github.com>
2021-10-25 20:30:53 +0300

    email

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,12 @@
 use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
+
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MailController;
-
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\HomePageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,14 +21,6 @@ use App\Http\Controllers\HomePageController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-
-Route::get('/', function () {
-    return view('front.index');
-});
 
 
 
@@ -37,28 +28,18 @@ Auth::routes();
 
 
 
-Route::name('NoMe.')->middleware('auth','checkRole')->group(function(){
-    Route::get('/', [HomeController::class ,'index'])->name('home');
-    Route::get('about', [HomeController::class ,'about'])->name('about');
-    Route::get('products', [HomeController::class ,'products'])->name('products');
-    Route::get('productpage', [HomeController::class ,'productpage'])->name('productpage');
-    Route::get('contact', [HomeController::class ,'contact'])->name('contact');
+
+Route::name('NoMe.')->group(function(){
+    Route::get('/',[HomeController::class ,'home'])->name('home');
+    Route::get('about',[HomeController::class ,'about'])->name('about');
+    Route::get('centers',[HomeController::class ,'centers'])->name('centers');
+    Route::get('products',[HomeController::class ,'products'])->name('products');
+    Route::get('productpage',[HomeController::class ,'productpage'])->name('productpage');
+    Route::get('contact',[HomeController::class ,'contact'])->name('contact');
     Route::post('send-email', [MailController::class, 'sendEmail'])->name('send-email');
     Route::get('send-email/index', [MailController::class, 'index'])->name('send-email.index');
 
 });
-// Route::prefix('NoMe')->name('NoMe.')->group(function(){
-//     Route::get('/',[HomePageController::class ,'home'])->name('home');
-//     Route::get('about',[HomePageController::class ,'about'])->name('about');
-//     Route::get('centers',[HomePageController::class ,'centers'])->name('centers');
-//     Route::get('products',[HomePageController::class ,'products'])->name('products');
-//     Route::get('productpage',[HomePageController::class ,'productpage'])->name('productpage');
-//     Route::get('contact',[HomePageController::class ,'contact'])->name('contact');
-//     Route::post('send-email', [MailController::class, 'sendEmail'])->name('send-email');
-//     Route::get('send-email/index', [MailController::class, 'index'])->name('send-email.index');
-//     // Route::post('send-email', [MailController::class, 'sendEmail'])->name('send-email');
-
-// });
 
 
 Route::prefix('admin')->middleware('auth','checkRole')->name('admin.')->group(function(){
